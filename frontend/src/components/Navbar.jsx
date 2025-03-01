01045f4e775eb0700d79b00bbcd260b498a16dfd
Rael Raelton <israeluyi7@gmail.com>
2025-03-01 04:11:06 +0100

    frontend

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import { Container, HStack, Text, Button, Flex } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { CiSquarePlus } from "react-icons/ci";
+
+import {useColorMode } from "./ui/color-mode";
+import { LuSun } from "react-icons/lu";
+import { IoMoonOutline  } from "react-icons/io5"
+
+const Navbar = () => {
+  const {colorMode, toggleColorMode} = useColorMode();
+  return (
+    <Container maxW="1140px" px={4}  >
+      <Flex
+        h={16}
+        alignItems="center" 
+        justifyContent="space-between"
+        flexDir={{ base: "column", sm: "row" }}
+      >
+        <Text
+          fontSize={{ base: 44, sm: 28 }}
+          fontWeight="bold"
+          textTransform="uppercase"
+          textAlign="center"
+          bgGradient="to-r" gradientFrom="red.400" gradientTo="blue.400"
+          bgClip="text"
+        >
+          <Link to="/">Product Store 🛒</Link>
+        </Text>
+
+        <HStack 
+          spacing={2} 
+          lignItems="center"
+          
+        >
+          <Link to="/create">
+            <Button>
+      
+              <CiSquarePlus  fontSize={20} />
+              
+            </Button>
+          </Link>
+
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <IoMoonOutline  /> : <LuSun size={20} /> }
+          </Button>
+        </HStack>
+      </Flex>
+    </Container>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/pages/CreatePage.jsx
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+import { Container, Heading, VStack, Box, Button, Input } from '@chakra-ui/react';
+import { useColorModeValue } from '@/components/ui/color-mode';
+import { useProductStore } from "../store/product"
+
+const CreatePage = () => {
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: ""
+  });
+
+  const {createProduct} = useProductStore()
+
+  const handleAddProduct = async () => {
+    const {success,message} = await createProduct(newProduct);
+    console.log("Success:", success); 
+    console.log("Message:", message); 
+  };
+  return (
+    <Container 
+      // maxW="600px" mx="auto" mt={20}
+      maxW={["90%", "80%", "600px"]}
+      p={[4, 6]}
+      mx="auto" 
+      mt={20}
+    
+    >
+      <VStack spacing={8}>
+        <Heading as="h1" size="2xl" textAlign="center" mb={8}
+        >
+          Create New Product
+        </Heading>
+         
+          <Box
+            w="full" bg={useColorModeValue("white", "gray.800")}
+            p={6} rounded="lg" shadow="md"
+          >
+            <VStack spacing={4}  >
+              <Input 
+                placeholder='Product Name'
+                name='name'
+                value={newProduct.name}
+                onChange={(e) => setNewProduct({...newProduct, name: e.target.value})}
+
+                border="1px solid gray"
+                borderRadius="md"
+               
+              
+
+              />
+
+              <Input
+                placeholder='Price'
+                name='price'
+                type='number'
+                value={newProduct.price}
+                onChange={(e) => setNewProduct({...newProduct, price: e.target.value})}
+
+                border="1px solid gray"
+                borderRadius="md"
+                
+              />
+
+              <Input
+                placeholder='Image URL'
+                name='image'
+                value={newProduct.image}
+                onChange={(e) => setNewProduct({...newProduct, image: e.target.value})}
+
+                border="1px solid gray"
+                borderRadius="md"
+                
+              />
+
+              <Button colorScheme="blue" onClick={handleAddProduct} w="full" > Add Product</Button>
+            </VStack>
+          </Box>
+        
+      </VStack>
+    </Container>
+  );
+};
+
+export default CreatePage
